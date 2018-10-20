48112e1cea3b371e534cbbaaf1c20d0ded9fbc7e
Stuart Baly <stuartbaly@gmail.com>
2018-10-20 12:29:38 +1100

    Code to toggle all I/O slowly for testing.

--- src/my-project.c
+++ src/my-project.c
@@ -7,44 +7,34 @@
 /* Do stuff with a Blue Pill.
 
 Current setup:
-  Red LED:      pin 17    (PB5 active low)
-  Orange LED:   pin 15    (PB7 active low)
-  On board LED:           (PB1 active high)
+    No LEDs or anything. Toggle all I/Os to see if they all work 18 hi, 19-20 lo, 12-14 lo
 */
 
 uint32_t ctr1;
-uint32_t ctr2;
-uint32_t ctr3;
 
 static void gpio_setup(void)
 {
 	/* Enable GPIOB clock. */
+	rcc_periph_clock_enable(RCC_GPIOA);
 	rcc_periph_clock_enable(RCC_GPIOB);
+	rcc_periph_clock_enable(RCC_GPIOC);
 
+	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
+		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO0|GPIO1|GPIO2|GPIO3|GPIO4|GPIO5|GPIO6|GPIO7|GPIO8|GPIO9|GPIO10|GPIO11|GPIO12|GPIO15);
 	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ,
-		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO1);
-	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ,
-		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO5);
-	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ,
-		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO7);
+		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO0|GPIO1|GPIO2|GPIO3|GPIO4|GPIO5|GPIO6|GPIO7|GPIO10|GPIO11|GPIO12|GPIO13|GPIO14|GPIO15);
+	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
+		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO13|GPIO14|GPIO15);
 }
 
 void sys_tick_handler(void)
 {
 
-	if (ctr1++ == 79) {
-		gpio_toggle(GPIOB, GPIO1);
+	if (ctr1++ == 2000) {
+  	gpio_toggle(GPIOA, GPIO0|GPIO1|GPIO2|GPIO3|GPIO4|GPIO5|GPIO6|GPIO7|GPIO8|GPIO9|GPIO10|GPIO11|GPIO12|GPIO15);
+  	gpio_toggle(GPIOB, GPIO0|GPIO1|GPIO2|GPIO3|GPIO4|GPIO5|GPIO6|GPIO7|GPIO10|GPIO11|GPIO12|GPIO13|GPIO14|GPIO15);
+  	gpio_toggle(GPIOC, GPIO13|GPIO14|GPIO15);
 		ctr1 = 0;
-	}
-
-	if (ctr2++ == 37) {
-		gpio_toggle(GPIOB, GPIO5);
-		ctr2 = 0;
-	}
-
-	if (ctr3++ == 53) {
-		gpio_toggle(GPIOB, GPIO7);
-		ctr3 = 0;
 	}
 
 }
@@ -53,11 +43,11 @@ int main(void) {
 	rcc_clock_setup_in_hse_16mhz_out_72mhz();
 	gpio_setup();
 
-	gpio_set(GPIOB, GPIO1);
-	gpio_clear(GPIOB, GPIO5);
-	gpio_clear(GPIOB, GPIO7);
+	gpio_clear(GPIOA, GPIO0|GPIO1|GPIO2|GPIO3|GPIO4|GPIO5|GPIO6|GPIO7|GPIO8|GPIO9|GPIO10|GPIO11|GPIO12|GPIO15);
+	gpio_clear(GPIOB, GPIO0|GPIO1|GPIO2|GPIO3|GPIO4|GPIO5|GPIO6|GPIO7|GPIO10|GPIO11|GPIO12|GPIO13|GPIO14|GPIO15);
+	gpio_clear(GPIOC, GPIO13|GPIO14|GPIO15);
 
-	ctr1 = ctr2 = ctr3 = 0;
+	ctr1 = 0;
 
 	/* 72MHz / 8 => 9000000 counts per second */
 	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
@@ -73,5 +63,10 @@ int main(void) {
 
 	while (1); /* Halt. */
 
+  //while (1) {
+  //	gpio_toggle(GPIOA, GPIO0|GPIO1|GPIO2|GPIO3|GPIO4|GPIO5|GPIO6|GPIO7|GPIO8|GPIO9|GPIO10|GPIO11|GPIO12|GPIO15);
+  //	gpio_toggle(GPIOB, GPIO0|GPIO1|GPIO2|GPIO3|GPIO4|GPIO5|GPIO6|GPIO7|GPIO10|GPIO11|GPIO12|GPIO13|GPIO14|GPIO15);
+  //	gpio_toggle(GPIOC, GPIO13|GPIO14|GPIO15);
+//} 
 	return 0;
 }
