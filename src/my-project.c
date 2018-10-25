b50bd1ffc067b12a662089ba520ce4ff61a8ee11
Stuart Baly <stuartbaly@gmail.com>
2018-10-25 21:48:28 +1100

    Corrected code for 8MHz crystal.

--- src/my-project.c
+++ src/my-project.c
@@ -1,75 +1,106 @@
 #include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/flash.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
 #include <libopencm3/cm3/nvic.h>
-#include <libopencm3/cm3/systick.h>
 
 /* Do stuff with a Blue Pill.
 
 Current setup:
-    No LEDs or anything. Toggle all I/Os to see if they all work.
-    Found that everything worked as expected, with the exception of the JTAG pins.
-    There's a register that can be set (AFIO_something?) that can disable the JTAG
-    or SWD pins as required.
+    Serial port on USART3
 */
 
-uint32_t ctr1;
+static void clock_setup(void)
+{
+	rcc_clock_setup_in_hse_8mhz_out_72mhz();
+
+	/* Enable GPIOB clock (for LED GPIOs). */
+	rcc_periph_clock_enable(RCC_GPIOB);
+
+	/* Enable clocks for GPIO port B (for GPIO_USART3_TX) and USART3. */
+	rcc_periph_clock_enable(RCC_AFIO);
+	rcc_periph_clock_enable(RCC_USART3);
+}
+
+static void usart_setup(void)
+{
+	/* Enable the USART2 interrupt. */
+	nvic_enable_irq(NVIC_USART3_IRQ);
+
+	/* Setup GPIO pin GPIO_USART3_TX on GPIO port B for transmit. */
+	gpio_set_mode(GPIO_BANK_USART3_TX, GPIO_MODE_OUTPUT_50_MHZ,
+		      GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART3_TX);
+
+	/* Setup GPIO pin GPIO_USART3_RX on GPIO port B for receive. */
+	gpio_set_mode(GPIO_BANK_USART3_RX, GPIO_MODE_INPUT,
+		      GPIO_CNF_INPUT_FLOAT, GPIO_USART3_RX);
+
+	/* Setup UART parameters. */
+	usart_set_baudrate(USART3, 115200);
+	usart_set_databits(USART3, 8);
+	usart_set_stopbits(USART3, USART_STOPBITS_1);
+	usart_set_parity(USART3, USART_PARITY_NONE);
+	usart_set_flow_control(USART3, USART_FLOWCONTROL_NONE);
+	usart_set_mode(USART3, USART_MODE_TX_RX);
+
+	/* Enable USART2 Receive interrupt. */
+	USART_CR1(USART3) |= USART_CR1_RXNEIE;
+
+	/* Finally enable the USART. */
+	usart_enable(USART3);
+}
 
 static void gpio_setup(void)
 {
-	/* Enable GPIOB clock. */
-	rcc_periph_clock_enable(RCC_GPIOA);
-	rcc_periph_clock_enable(RCC_GPIOB);
-	rcc_periph_clock_enable(RCC_GPIOC);
 
-	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_2_MHZ,
-		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO0|GPIO1|GPIO2|GPIO3|GPIO4|GPIO5|GPIO6|GPIO7|GPIO8|GPIO9|GPIO10|GPIO11|GPIO12|GPIO15);
+	/* Setup PB1 for LED use. */
 	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ,
-		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO0|GPIO1|GPIO2|GPIO3|GPIO4|GPIO5|GPIO6|GPIO7|GPIO10|GPIO11|GPIO12|GPIO13|GPIO14|GPIO15);
-	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
-		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO13|GPIO14|GPIO15);
+		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO1);
+	
+	gpio_clear(GPIOB, GPIO1);
 }
 
-void sys_tick_handler(void)
+void usart3_isr(void)
 {
+	static uint8_t data = 'A';
 
-	if (ctr1++ == 2000) {
-  	gpio_toggle(GPIOA, GPIO0|GPIO1|GPIO2|GPIO3|GPIO4|GPIO5|GPIO6|GPIO7|GPIO8|GPIO9|GPIO10|GPIO11|GPIO12|GPIO15);
-  	gpio_toggle(GPIOB, GPIO0|GPIO1|GPIO2|GPIO3|GPIO4|GPIO5|GPIO6|GPIO7|GPIO10|GPIO11|GPIO12|GPIO13|GPIO14|GPIO15);
-  	gpio_toggle(GPIOC, GPIO13|GPIO14|GPIO15);
-		ctr1 = 0;
+	/* Check if we were called because of RXNE. */
+	if (((USART_CR1(USART3) & USART_CR1_RXNEIE) != 0) &&
+	    ((USART_SR(USART3) & USART_SR_RXNE) != 0)) {
+
+		/* Indicate that we got data. */
+		gpio_toggle(GPIOB, GPIO1);
+
+		/* Retrieve the data from the peripheral. */
+		data = usart_recv(USART3);
+
+		/* Enable transmit interrupt so it sends back the data. */
+		USART_CR1(USART3) |= USART_CR1_TXEIE;
 	}
 
+	/* Check if we were called because of TXE. */
+	if (((USART_CR1(USART3) & USART_CR1_TXEIE) != 0) &&
+	    ((USART_SR(USART3) & USART_SR_TXE) != 0)) {
+
+		/* Indicate that we are sending out data. */
+		//gpio_toggle(GPIOB, GPIO1);
+
+		/* Put data into the transmit register. */
+		usart_send(USART3, data);
+
+		/* Disable the TXE interrupt as we don't need it anymore. */
+		USART_CR1(USART3) &= ~USART_CR1_TXEIE;
+	}
 }
 
-int main(void) {
-	rcc_clock_setup_in_hse_16mhz_out_72mhz();
+int main(void)
+{
+	clock_setup();
 	gpio_setup();
+	usart_setup();
 
-	gpio_clear(GPIOA, GPIO0|GPIO1|GPIO2|GPIO3|GPIO4|GPIO5|GPIO6|GPIO7|GPIO8|GPIO9|GPIO10|GPIO11|GPIO12|GPIO15);
-	gpio_clear(GPIOB, GPIO0|GPIO1|GPIO2|GPIO3|GPIO4|GPIO5|GPIO6|GPIO7|GPIO10|GPIO11|GPIO12|GPIO13|GPIO14|GPIO15);
-	gpio_clear(GPIOC, GPIO13|GPIO14|GPIO15);
+	/* Wait forever and do nothing. */
+	while (1)
+		__asm__("nop");
 
-	ctr1 = 0;
-
-	/* 72MHz / 8 => 9000000 counts per second */
-	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
-
-	/* 9000000/9000 = 1000 overflows per second - every 1ms one interrupt */
-	/* SysTick interrupt every N clock pulses: set reload to N-1 */
-	systick_set_reload(8999);
-
-	systick_interrupt_enable();
-
-	/* Start counting. */
-	systick_counter_enable();
-
-	while (1); /* Halt. */
-
-  //while (1) {
-  //	gpio_toggle(GPIOA, GPIO0|GPIO1|GPIO2|GPIO3|GPIO4|GPIO5|GPIO6|GPIO7|GPIO8|GPIO9|GPIO10|GPIO11|GPIO12|GPIO15);
-  //	gpio_toggle(GPIOB, GPIO0|GPIO1|GPIO2|GPIO3|GPIO4|GPIO5|GPIO6|GPIO7|GPIO10|GPIO11|GPIO12|GPIO13|GPIO14|GPIO15);
-  //	gpio_toggle(GPIOC, GPIO13|GPIO14|GPIO15);
-//} 
 	return 0;
 }
