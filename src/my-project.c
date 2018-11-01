a46a6ac6885cafaf56112ec9ba98c4669ecfff82
Stuart Baly <stuartbaly@gmail.com>
2018-11-01 22:19:50 +1100

    Rx char on USART3, echo back hex. Enter=0x0D.

--- src/my-project.c
+++ src/my-project.c
@@ -10,10 +10,14 @@
 
 Current setup:
     Serial port on USART3
+    
+    Receive chars on USART3
+    Echo hex of chars out
+    PB1 high=on
 */
 /* ==================================================================== */
 /*   RINGBUFFER                                                         */
-
+/*
 struct ring {
 	uint8_t *data;
 	uint32_t size;
@@ -65,25 +69,67 @@ static int32_t ring_read_ch(struct ring *ring, uint8_t *ch)
 
 	return ret;
 }
-
+*/
 /* ==================================================================== */
 
+static void clock_setup(void)
+{
+	rcc_clock_setup_in_hse_8mhz_out_72mhz();
 
-#define BUFFER_SIZE 16
-uint8_t  serbuf[BUFFER_SIZE];
-struct ring myring;
-uint8_t x;
-uint32_t ret;
+	/* Enable GPIOC clock. */
+	rcc_periph_clock_enable(RCC_GPIOB);
+
+	/* Enable clocks for GPIO port B (for GPIO_USART3_TX) and USART3. */
+	rcc_periph_clock_enable(RCC_USART3);
+}
+static void usart_setup(void)
+{
+	/* Setup GPIO pin GPIO_USART3_TX. */
+	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
+		      GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART3_TX);
+
+	/* Setup UART parameters. */
+	usart_set_baudrate(USART3, 9600);
+	usart_set_databits(USART3, 8);
+	usart_set_stopbits(USART3, USART_STOPBITS_1);
+	usart_set_mode(USART3, USART_MODE_TX_RX);
+	usart_set_parity(USART3, USART_PARITY_NONE);
+	usart_set_flow_control(USART3, USART_FLOWCONTROL_NONE);
+
+	/* Finally enable the USART. */
+	usart_enable(USART3);
+}
+
+static void gpio_setup(void)
+{
+	/* Set GPIO1 (in GPIO port B) to 'output push-pull'. */
+	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ,
+		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO1);
+}
+
+uint8_t hexchar(uint8_t c) {
+  c+=48;   /* convert 0-9 to '0'-'9' */
+  if (c>'9') {
+    c+=7;
+  }
+  return c;
+}
+
 
 int main(void)
 {
-    while (1) {
-      ring_init( &myring, serbuf, BUFFER_SIZE );
-      ring_write_ch( &myring, 'A');
-      ring_write_ch( &myring, 'B');
-      ring_write_ch( &myring, 'C');
-      ret=ring_read_ch( &myring, &x);
-    }
-
+  uint8_t x;
+  
+  clock_setup();
+  gpio_setup();
+  usart_setup();
+  
+  while(1) {
+    x=usart_recv_blocking(USART3);
+    usart_send_blocking(USART3, hexchar((x>>4) & 0x0f ));
+    usart_send_blocking(USART3, hexchar(x & 0x0f ));
+    usart_send_blocking(USART3, '\r');
+    usart_send_blocking(USART3, '\n');
+  }    
 }
 
